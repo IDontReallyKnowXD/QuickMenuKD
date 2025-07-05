6bff2047848452c15363186746d732513f4bf2a4
IDontReallyKnowXD <jycgjycplay@gmail.com>
2025-07-05 11:39:22 +0300

    name changes

--- Assets/Scripts/ItemsList.cs
+++ Assets/Scripts/ItemsList.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/QMlistScriptableObject", order = 1)]
 public class ItemsList : ScriptableObject //might need to change to singular items
 {
-    public List<Item> items;
+    public List<ItemQM> items;
 }
 
 [Serializable]
-public class Item
+public class ItemQM
 {
     public int id;
     public int uses;
