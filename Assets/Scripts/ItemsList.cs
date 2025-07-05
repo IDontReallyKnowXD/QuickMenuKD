26c888e751d485c394cb1b2cda9fc9ecda4efbd7
IDontReallyKnowXD <jycgjycplay@gmail.com>
2025-07-05 11:37:12 +0300

    /

--- Assets/Scripts/ItemsList.cs
+++ Assets/Scripts/ItemsList.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/QMlistScriptableObject", order = 1)]
-public class ItemsList : ScriptableObject 
+public class ItemsList : ScriptableObject //might need to change to singular items
 {
     public List<Item> items;
 }
